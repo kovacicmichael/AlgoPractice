f1c386d71aa40607333bad0a0549177f0bdf8599
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-05-30 16:18:45 -0600

    maxchar practice

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,34 +4,59 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {
-	const charOb = {};
-	let max = 0;
-	let maxChar = "";
+// function maxChar(str) {
+// 	const charOb = {};
+// 	let max = 0;
+// 	let maxChar = "";
+
+// 	for (let char of str){
+// 		//charOb[char] = charOb[char] + 1 || 1;
+// 		if(!charOb[char]){
+// 			charOb[char] = 1;
+// 		}else{
+// 			charOb[char]++
+// 		}
+// 	}
+
+// 	for (let letter in charOb){
+// 		if(charOb[letter] > max){
+// 			max = charOb[letter];
+// 			maxChar = letter;
+// 		}
+		
+// 	}
+// 	console.log(max);
+// 	console.log(maxChar);
+// 	return maxChar;
+// }
+
+
+
+
+//maxChar("hello world")
+//module.exports = maxChar;
+
+
+function charMap (str){
+
+	const charMap = {};
 
 	for (let char of str){
-		//charOb[char] = charOb[char] + 1 || 1;
-		if(!charOb[char]){
-			charOb[char] = 1;
+		console.log(char);
+		if(!charMap[char]){
+			charMap[char] = 1;
 		}else{
-			charOb[char]++
+			charMap[char]++
 		}
 	}
-
-	for (let letter in charOb){
-		if(charOb[letter] > max){
-			max = charOb[letter];
-			maxChar = letter;
-		}
-		
+	for (let char in charMap){
+		console.log("Letter: " + char + " occurs " + charMap[char] + " times.");
 	}
-	console.log(max);
-	console.log(maxChar);
-	return maxChar;
+	console.log(charMap)
 }
 
+charMap("this is it")
 
 
 
-maxChar("hello world")
-module.exports = maxChar;
+
