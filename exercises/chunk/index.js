0a2a5a9ed7d95c7c27c6d099b3c959f93a1329ab
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-06-18 14:19:03 -0600

    new exercise

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -12,24 +12,24 @@ function chunk(array, size) {
 	const chunked = [];
 	let index = 0;
 
-	while(index < array.length){
-		chunked.push(array.slice(index, index + size));
-		index += size;
-	}
-	return chunked;
-	// const chunked = [];
-
-	// for (let element of array){
-	// 	const last = chunked[chunked.length - 1];
-	// 	console.log(last)
-	// 	if(!last || last.length === size){
-	// 		chunked.push([element])
-	// 	}else{
-	// 		last.push(element);
-	// 	}
+	// while(index < array.length){
+	// 	chunked.push(array.slice(index, index + size));
+	// 	index += size;
 	// }
-
 	// return chunked;
+	const chunked = [];
+
+	for (let element of array){
+		const last = chunked[chunked.length - 1];
+		console.log(last)
+		if(!last || last.length === size){
+			chunked.push([element])
+		}else{
+			last.push(element);
+		}
+	}
+
+	return chunked;
 }
 
 module.exports = chunk;
